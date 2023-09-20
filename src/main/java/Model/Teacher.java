7d99fa2aa9be676dd6d365d97af33738970b6805
harriszk <zacharykeatonharris@gmail.com>
2023-09-20 16:35:31 -0400

    Feat: Added getTeacherById tests.

--- src/main/java/Model/Teacher.java
+++ src/main/java/Model/Teacher.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.Objects;
+
 public class Teacher {
     private int id;
     private String name;
@@ -31,5 +33,18 @@ public class Teacher {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Teacher teacher = (Teacher) o;
+        return id == teacher.id && name.equals(teacher.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
---
+++ src/test/java/TeacherDAOTest.java
@@ -0,0 +1,34 @@
+import DAO.TeacherDAO;
+import Model.Teacher;
+import Util.ConnectionSingleton;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+
+public class TeacherDAOTest {
+    private TeacherDAO teacherDAO;
+
+    @Before
+    public void setUp() {
+        Connection conn = ConnectionSingleton.getConnection();
+        teacherDAO = new TeacherDAO(conn);
+    }
+
+    @Test
+    public void getTeacherByIdSuccessfulTest() {
+        Teacher expected = new Teacher(1, "Zachary Harris");
+        Teacher actual = teacherDAO.getTeacherById(1);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getTeacherByIdUnsuccessfulTest() {
+        Teacher actual = teacherDAO.getTeacherById(-1);
+        Assert.assertNull(actual);
+        //Assert.assertThrows(Exception.class, ()->teacherDAO.getTeacherById(-1));
+    }
+
+    
+}
