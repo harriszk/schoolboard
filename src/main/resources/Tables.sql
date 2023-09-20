61557f8f8ba328e5b7b22e1de044b22346f2c55a
harriszk <zacharykeatonharris@gmail.com>
2023-09-20 15:30:14 -0400

    Added SQL code

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -1,3 +1,43 @@
 --h2 is typically used to setup a test database, not a prod database.
 --first, drop your tables (to reset your database for testing)
 --then create your tables
+
+CREATE TABLE Student
+(
+  id INT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Teacher
+(
+  id INT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE Course
+(
+  id INT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE StudentCourses
+(
+  student_id INT NOT NULL,
+  course_id INT NOT NULL,
+  PRIMARY KEY (student_id, course_id),
+  FOREIGN KEY (student_id) REFERENCES Student(id),
+  FOREIGN KEY (course_id) REFERENCES Courses(id)
+);
+
+CREATE TABLE TeacherCourses
+(
+  teacher_id INT NOT NULL,
+  course_id INT NOT NULL,
+  PRIMARY KEY (teacher_id, course_id),
+  FOREIGN KEY (course_id) REFERENCES Courses(id),
+  FOREIGN KEY (teacher_id) REFERENCES Teacher(id)
+);
