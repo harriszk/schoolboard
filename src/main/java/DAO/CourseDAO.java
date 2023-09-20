20a849b6e88e33a6453742cb13885ac39883103b
Ralph <raftsdev@gmail.com>
2023-09-20 18:12:28 -0400

    Feat: Fills with methods with FALSE return state

--- src/main/java/DAO/CourseDAO.java
+++ src/main/java/DAO/CourseDAO.java
@@ -1,4 +1,58 @@
 package DAO;
 
+import Model.Course;
+import Model.Teacher;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CourseDAO {
+
+    Connection conn;
+    public CourseDAO(Connection conn){
+        this.conn = conn;
+    }
+
+    public List<Course> getAllCourses(){
+        List<Course> courses = new ArrayList<Course>();
+        try {
+
+
+            PreparedStatement ps = conn.prepareStatement("select * from course");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                 courses.add(new Course(rs.getInt("id"),rs.getString("name")));
+            }
+
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return courses;
+    }
+
+    public Course getCourseById(int id){
+        return null;
+    }
+
+    public boolean addCourse(Course course){
+        return false;
+    }
+
+    public boolean deleteCourse(int id){
+        return false;
+    }
+
+    public boolean updateCourse(int id, String courseName){
+        return false;
+    }
+
+
+
+
+
+
 }
