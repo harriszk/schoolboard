b393196f87d0e86264a3b265346baf1a4027623a
Ralph <raftsdev@gmail.com>
2023-09-20 17:24:08 -0400

    Feat: Add console interface

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,6 +2,9 @@ import Controller.Controller;
 import DAO.TeacherDAO;
 import Util.ConnectionSingleton;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
 //        this line is just for testing that your tables get set up correctly
@@ -15,5 +18,33 @@ public class Application {
         TeacherDAO teacherDAO = new TeacherDAO(ConnectionSingleton.getConnection());
         System.out.println("Teacher with id=1"+ teacherDAO.getTeacherById(1));*/
 
+        // Console interface
+        Scanner sc = new Scanner(System.in);
+        boolean theEnd = false;
+        while(!theEnd){
+            System.out.println("1: Find a Teacher by id. 2: Exit");
+            int choice = Integer.parseInt(sc.nextLine());
+            if(choice == 1){
+                System.out.println("Enter id of teacher:");
+                int id = Integer.parseInt(sc.nextLine());
+                TeacherDAO teacherDAO = new TeacherDAO(ConnectionSingleton.getConnection());
+                System.out.println("Teacher with id="+id+": "+ teacherDAO.getTeacherById(id));
+            }else if(choice == 2){
+                theEnd=true;
+            }else if(choice ==3) {
+
+                System.out.println("invalid choice");
+            }else if(choice == 4){
+                System.out.println("invalid choice");
+
+            }else if(choice == 5){
+                System.out.println("invalid choice");
+
+            }else if(choice == 6){
+                System.out.println("invalid choice");
+            }else{
+                System.out.println("invalid choice");
+            }
+        }
     }
 }
