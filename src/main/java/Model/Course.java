c67a3682cf0c469544dceea6c499b8efd400d55d
Ralph <raftsdev@gmail.com>
2023-09-20 15:38:45 -0400

    Feat: Add Teacher, Student, Course tables

---
+++ src/main/java/Model/Course.java
@@ -0,0 +1,35 @@
+package Model;
+
+public class Course {
+    private int id;
+    private String course;
+
+    public Course(int id, String course) {
+        this.id = id;
+        this.course = course;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", course='" + course + '\'' +
+                '}';
+    }
+}
