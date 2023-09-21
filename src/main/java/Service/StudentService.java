32dbae4a54faef75a619f3004e21a465f47d92b8
Ralph <raftsdev@gmail.com>
2023-09-21 16:55:01 -0400

    Feat: Add methods to StudentService

--- src/main/java/Service/StudentService.java
+++ src/main/java/Service/StudentService.java
@@ -1,17 +1,32 @@
 package Service;
 
 import DAO.StudentDAO;
+import Model.Student;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class StudentService {
     private StudentDAO studentDAO;
 
-    public StudentService(Connection conn) {
-        this.studentDAO = new StudentDAO(conn);
-    }
-
     public StudentService(StudentDAO studentDAO) {
         this.studentDAO = studentDAO;
     }
+
+    public List<Student> getAllStudents(){
+        return studentDAO.getAllStudents();
+    }
+
+    public Student getStudentById(int Id){
+        return studentDAO.getStudentById(Id);
+    }
+    public boolean addStudent(Student student){
+        return studentDAO.addStudent(student);
+    }
+
+    public boolean deleteStudentById(int Id){
+        return studentDAO.deleteStudentById(Id);
+    }
+
+
 }
