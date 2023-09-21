ed2441fe5b526d2bb1c0db211b36c8caf803ce5d
Ralph <raftsdev@gmail.com>
2023-09-21 15:23:21 -0400

    Fix: Overwrite Student.Equals()

--- src/main/java/Model/Student.java
+++ src/main/java/Model/Student.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.Objects;
+
 public class Student {
     private int id;
     private String name;
@@ -39,6 +41,18 @@ public class Student {
         this.email = email;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && Objects.equals(name, student.name) && Objects.equals(email, student.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
 
     @Override
     public String toString() {
