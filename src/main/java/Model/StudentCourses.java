608ed1675dc7d828c2e40e8274acfaf00e24bd78
harriszk <zacharykeatonharris@gmail.com>
2023-09-22 16:18:00 -0400

    Feat: Added getters and setters

--- src/main/java/Model/StudentCourses.java
+++ src/main/java/Model/StudentCourses.java
@@ -1,5 +1,35 @@
 package Model;
 
 public class StudentCourses {
-    
+    private int studentId;
+    private int courseId;
+
+    public StudentCourses(int studentId, int courseId) {
+        this.studentId = studentId;
+        this.courseId = courseId;
+    }
+
+    public int getStudentId() {
+        return this.studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getCourseId() {
+        return this.courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " studentId='" + getStudentId() + "'" +
+            ", courseId='" + getCourseId() + "'" +
+            "}";
+    }    
 }
