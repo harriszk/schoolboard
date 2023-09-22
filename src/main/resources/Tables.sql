09241468cc35222c1bc2e9498a5642aa060089fa
harriszk <zacharykeatonharris@gmail.com>
2023-09-22 09:23:44 -0400

    Fix: Updated database setup

--- src/main/resources/Tables.sql
+++ src/main/resources/Tables.sql
@@ -4,16 +4,15 @@
 
 
 drop table StudentCourses if exists;
-drop table TeacherCourses if exists;
 drop table Student if exists;
-drop table Teacher if exists;
 drop table Course if exists;
+drop table Teacher if exists;
 
 -- Create a schema
 
 CREATE TABLE Student
 (
-  id INT PRIMARY KEY,
+  id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
@@ -21,16 +20,18 @@ CREATE TABLE Student
 
 CREATE TABLE Teacher
 (
-  id INT PRIMARY KEY,
+  id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE Course
 (
-  id INT PRIMARY KEY,
+  id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id)
+  teacher_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (teacher_id) REFERENCES Teacher(id)
 );
 
 CREATE TABLE StudentCourses
@@ -41,16 +42,6 @@ CREATE TABLE StudentCourses
   FOREIGN KEY (student_id) REFERENCES Student(id),
   FOREIGN KEY (course_id) REFERENCES Course(id)
 );
-
-CREATE TABLE TeacherCourses
-(
-  teacher_id INT NOT NULL,
-  course_id INT NOT NULL,
-  PRIMARY KEY (teacher_id, course_id),
-  FOREIGN KEY (course_id) REFERENCES Course(id),
-  FOREIGN KEY (teacher_id) REFERENCES Teacher(id)
-);
-
 
 -- Fill a data
 
@@ -65,4 +56,21 @@ insert into course (id, name) values (3, 'Databases');
 insert into course (id, name) values (4, 'English');
 insert into course (id, name) values (5, 'Physics');
 
---
+-- Student table
+insert into student (id, name, email) values (1, 'John Doe', 'johnD@someCompany.com');
+insert into student (id, name, email) values (2, 'Jane Doe', 'janeD@someCompany.com');
+insert into student (id, name, email) values (3, 'Daisy Moyer', 'DaisyMoyer@CrystalEngineer.com');
+/*
+INSERT INTO Student (id, name, email) values (4, 'Raymond Welsh', 'rwelsh@email.com');
+INSERT INTO Student (id, name, email) values (5, 'Maya Lam', 'mlam@email.com');
+INSERT INTO Student (id, name, email) values (6, 'Mae Garza', 'mgarza@email.com');
+INSERT INTO Student (id, name, email) values (7, 'Eve Henry', 'ehenry@email.com');
+INSERT INTO Student (id, name, email) values (8, 'Gracie Webster', 'gwebster@email.com');
+INSERT INTO Student (id, name, email) values (9, 'Angus Khan', 'akhan@email.com');
+INSERT INTO Student (id, name, email) values (10, 'Rodney Frazier', 'rfrazier@email.com');
+INSERT INTO Student (id, name, email) values (11, 'Calum Jensen', 'cjensen@email.com');
+INSERT INTO Student (id, name, email) values (12, 'Stephanie Carson', 'scarson@email.com');
+INSERT INTO Student (id, name, email) values (13, 'Ricardo George', 'rgeorge@email.com');
+INSERT INTO Student (id, name, email) values (14, 'Trey Gregory', 'tgregory@email.com');
+INSERT INTO Student (id, name, email) values (15, 'Tommy Barton', 'tbarton@email.com');
+*/
