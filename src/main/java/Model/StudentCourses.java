be472b473ba2dd33ae55b1889a87eb94c1cf1061
harriszk <zacharykeatonharris@gmail.com>
2023-09-22 17:26:47 -0400

    Feat: StudentCourses equals method

--- src/main/java/Model/StudentCourses.java
+++ src/main/java/Model/StudentCourses.java
@@ -1,5 +1,7 @@
 package Model;
 
+import java.util.Objects;
+
 public class StudentCourses {
     private int studentId;
     private int courseId;
@@ -23,6 +25,19 @@ public class StudentCourses {
 
     public void setCourseId(int courseId) {
         this.courseId = courseId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StudentCourses that = (StudentCourses) o;
+        return studentId == that.studentId && courseId == that.courseId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId, courseId);
     }
 
     @Override
