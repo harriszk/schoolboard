1caaf0152e6fed268c4c45562f0cf901f25c29bd
harriszk <zacharykeatonharris@gmail.com>
2023-09-25 14:52:32 -0400

    Fix: Added default constructor for teacher

--- src/main/java/Model/Teacher.java
+++ src/main/java/Model/Teacher.java
@@ -6,6 +6,10 @@ public class Teacher {
     private int id;
     private String name;
 
+    public Teacher() {
+
+    }
+
     public Teacher(int id, String name) {
         this.id = id;
         this.name = name;
