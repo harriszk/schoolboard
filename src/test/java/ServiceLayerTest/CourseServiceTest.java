f3a8735d16f22624e1e6b11da44f6f8a1941d58f
Ralph <raftsdev@gmail.com>
2023-09-25 17:53:42 -0400

    Feat: start the CourseServiceTest class

--- src/test/java/ServiceLayerTest/CourseServiceTest.java
+++ src/test/java/ServiceLayerTest/CourseServiceTest.java
@@ -1,4 +1,21 @@
 package ServiceLayerTest;
 
+import DAO.CourseDAO;
+import Service.CourseService;
+import org.junit.Before;
+import org.mockito.Mockito;
+
 public class CourseServiceTest {
+
+    CourseService courseService;
+    CourseDAO mockCourseDAO;
+
+    @Before
+    public void setUp(){
+        mockCourseDAO = Mockito.mock(CourseDAO.class);
+        courseService = new CourseService(mockCourseDAO);
+    }
+
+
+
 }
